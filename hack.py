75b0309197362d6a04591e0eddb3ea8e0765bdfb
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-11-30 14:20:21 +0300

    Stage 2/5: Simple brute force

--- hack.py
+++ hack.py
@@ -1,22 +1,38 @@
 # write your code here
+import itertools
 import sys
 import socket
+import string
+
+
+def password_generator():
+    letters_list = list(string.ascii_lowercase)
+    numbers_list = list(range(10))
+    the_list = letters_list + numbers_list
+    for i in range(1, len(the_list) + 1):
+        combinations = itertools.product(the_list, repeat=i)
+        for j in combinations:
+            password = ''
+            for k in j:
+                password += str(k)
+            yield password
+
 
 args = sys.argv
 hostname = args[1]
 port = int(args[2])
-msg_to_send = args[3]
 
 with socket.socket() as client_socket:
     address = (hostname, port)
-
     client_socket.connect(address)
+    attempt = password_generator()
 
-    data = msg_to_send.encode()
-
-    client_socket.send(data)
-
-    response = client_socket.recv(1024)
-
-    response = response.decode()
-    print(response)
+    for j in attempt:
+        data = j.encode('utf-8')
+        client_socket.send(data)
+        response = client_socket.recv(1024)
+        response = response.decode('utf-8')
+        if response == 'Connection success!':
+            print(j)
+            client_socket.close()
+            break
